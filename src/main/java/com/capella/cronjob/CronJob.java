cc8533c1d1a80566573dfa5ad03abc208ceedb34
mahmutcopoglu <mahmutcopoglu@gmail.com>
2024-02-17 11:17:12 +0300

    main

--- src/main/java/com/capella/cronjob/CronJob.java
+++ src/main/java/com/capella/cronjob/CronJob.java
@@ -1,9 +1,16 @@
 package com.capella.cronjob;
 
 import com.capella.domain.data.user.JwtUserData;
+import com.capella.domain.enums.CronJobStatus;
+import com.capella.domain.model.cronjob.CronJobModel;
+import com.capella.domain.model.cronjoblog.CronJobLogModel;
 import com.capella.security.constant.AuthorizationConstants;
+import com.capella.service.cronjob.CronJobService;
+import com.capella.service.model.ModelService;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -18,7 +25,18 @@ import java.util.UUID;
 @Getter
 public class CronJob implements Runnable{
 
+    @Autowired
+    protected CronJobService cronJobService;
+    @Autowired
+    protected ModelService modelService;
+
+    @Getter
+    @Setter
+    protected CronJobModel cronJobModel;
+
     public final static String CRONJOB_USER = "cronjob_user";
+    public final static String JOB_START = "Job başladı";
+    public final static String JOB_END = "Job bitti";
 
     @Override
     public void run() {
@@ -35,5 +53,13 @@ public class CronJob implements Runnable{
             token.setDetails(jwtUserData);
             SecurityContextHolder.getContext().setAuthentication(token);
         }
+    }
+
+    protected void saveCronJobLog(final CronJobModel cronJobModel, final CronJobLogModel cronJobLogModel,
+                                  final CronJobStatus cronJobStatus, final String logStatusDescription){
+        cronJobLogModel.setCronJob(cronJobModel);
+        cronJobLogModel.setLogStatus(cronJobStatus);
+        cronJobLogModel.setLogStatusDescription(logStatusDescription);
+        modelService.save(cronJobLogModel);
     }
 }
--- src/main/java/com/capella/cronjob/impl/ExchangeCronJob.java
+++ src/main/java/com/capella/cronjob/impl/ExchangeCronJob.java
@@ -1,10 +1,50 @@
 package com.capella.cronjob.impl;
 
 import com.capella.cronjob.CronJob;
+import com.capella.domain.enums.CronJobStatus;
+import com.capella.domain.model.cronjoblog.CronJobLogModel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
 public class ExchangeCronJob extends CronJob {
+    public final static String CRONJOB_NAME = "exchange_recorder";
+
+    @Value("${tcmb.exchange.xml}")
+    private String exchangeXml;
+
+    @Override
+    public void run() {
+        super.run();
+
+        this.cronJobModel = cronJobService.getCronJobModel(cronJobModel.getCode());
+        var cronJobLogModel = modelService.create(CronJobLogModel.class);
+        cronJobLogModel.setCode(UUID.randomUUID().toString());
+        var startDate = LocalDateTime.now();
+        try {
+            if(!CronJobStatus.STOPPED.equals(cronJobModel.getStatus())){
+                log.info(CRONJOB_NAME + " job başladı");
+                saveCronJobLog(cronJobModel, cronJobLogModel, CronJobStatus.RUNNING, JOB_START);
+                cronJobModel.setStatus(CronJobStatus.RUNNING);
+                modelService.save(cronJobModel);
+
+                //cron job işlemi
+
+                saveCronJobLog(cronJobModel, cronJobLogModel, CronJobStatus.SUCCESSFUL, JOB_END);
+                cronJobModel.setStatus(CronJobStatus.SUCCESSFUL);
+                modelService.save(cronJobModel);
+                log.info(CRONJOB_NAME + " job başarıyla tamalandı");
+            }else{
+                log.info(CRONJOB_NAME + " durumu DURDURULDU olan bir job başlayamaz.");
+            }
+        }catch (Throwable e){
+            cronJobModel.setStatus(CronJobStatus.FAILED);
+            modelService.save(cronJobModel);
+        }
+    }
 }
